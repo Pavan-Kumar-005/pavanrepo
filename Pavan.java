d5744780b67f6dc5cc6e2ae2f700f33f962bdfb9
Pavan Kumar <pavankumars2601@gmail.com>
2024-09-07 23:42:26 +0530

    new project

---
+++ Pavan.java
@@ -0,0 +1,17 @@
+package pavanrepo;
+public class Pavan {
+    public static void main(String[] args) {
+        f1();
+
+        System.out.println("hello manju bro");
+    }
+
+    public static void f1(){
+        System.out.println("hello there!!!");
+    }
+
+
+    
+    
+}
+
